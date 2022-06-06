4e69b0dbccda89c98609e033b2e84849b4e16a4e
cleo-cyber <kadimacleo92@gmail.com>
2022-06-06 05:35:16 +0300

    Add endpoints for getting ,deleteing ,searching and posting questions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -12,7 +13,14 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app)
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Headers","Content-Type,Authorization,true")
+        response.headers.add('Access-Control-Allow-Methods','GET,POST,OPTIONS,DELETE')
+
+        return response
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
@@ -26,6 +34,18 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories',methods=['GET'])
+    def get_all_questions():
+        try:
+            categories=Category.query.order_by(Category.id).all()
+            formatted_categories=[category.format() for category in categories]
+            return jsonify({
+                "success":True,
+                "categories":formatted_categories,
+                "total_categories":len(categories)
+            })
+        except:
+            abort(404)
 
 
     """
@@ -40,7 +60,22 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
-
+    @app.route('/questions',methods=['GET'])
+    def get_paginated_questions():
+        try:
+            page=request.args.get('page',1,type=int)
+            start=(page -1) * QUESTIONS_PER_PAGE
+            end=start + QUESTIONS_PER_PAGE
+            questions=Question.query.order_by(Question.id).all()
+            current_category=[question.format() for question in questions]
+            return jsonify({
+                "success":True,
+                "books":current_category[start:end],
+                "total_questions":len(Question.query.all())
+            })
+        except:
+            abort(404)
+    
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
@@ -48,18 +83,50 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
-
+    @app.route('/questions/<int:question_id>',methods=['DELETE'])
+    def delete_question(question_id):
+        question=Question.query.filter(Question.id==question_id).one_or_none()
+        if question is None:
+            abort(422)
+        question.delete()
+        return jsonify({
+            "success":True,
+            "delete_id":question.id,
+            "total_questions":len(Question.query.all())
+        })
     """
     @TODO:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
     category, and difficulty score.
-
+    
     TEST: When you submit a question on the "Add" tab,
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route('/questions',methods=['POST'])
+    def create_question():
+        body=request.get_json()
+        add_question=body.get('question',None)
+        new_answer=body.get('answer',None)
+        question_category=body.get('answer',None)
+        new_difficulty_score=body.get('difficulty',None)
+        try:
+            new_question=Question(
+                question=add_question,
+                answer=new_answer,
+                category=question_category,
+                difficulty=new_difficulty_score
+            )
+            new_question.insert()
+            return jsonify({
+                "success":True,
+                "created":new_question.id,
+                "total_questions":len(Question.query.all())
 
+            })
+        except:
+            abort(405)
     """
     @TODO:
     Create a POST endpoint to get questions based on a search term.
@@ -70,7 +137,24 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
-
+    @app.route('/questions/search',methods=['POST'])
+    def search_question():
+        body=request.get_json()
+        search_term=body.get('searchTerm',None)
+        try:
+            if search_term:
+                page=request.args.get('page',1,type=int)
+                start=(page -1)* QUESTIONS_PER_PAGE
+                end=start +QUESTIONS_PER_PAGE
+                search_query=Question.query.filter_by(Question.question.ilike('%'+search_term+'%')).all()
+                formatted_search=[question.format() for question in search_query]
+                return jsonify({
+                    "success":True,
+                    "questions":formatted_search[start:end],
+                    "total_results":len(search_query)
+                })
+        except:
+            abort(404)
     """
     @TODO:
     Create a GET endpoint to get questions based on category.
@@ -79,7 +163,15 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>/questions',methods=['GET'])
+    def get_question_based_on_category(category_id):
+        questions=Question.query.filter(Question.category==category_id).all()
 
+        return jsonify({
+            "success":True,
+            "questions":[question.format() for question in questions],
+            "total_questions":len(questions)
+        })
     """
     @TODO:
     Create a POST endpoint to get questions to play the quiz.
--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
+password=5663
+database_path = 'postgres://{}:{}@{}/{}'.format('postgres',password,'localhost:5432', database_name)
+
 
 db = SQLAlchemy()
 
