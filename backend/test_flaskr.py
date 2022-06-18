8698497469e9a2ba10c69fdfb106a962a8436fe3
fodela <particleme30@gmail.com>
2022-06-18 20:32:58 +0000

    feat: test get_categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+from dotenv import load_dotenv
+
+# look for a file named .env and load my database username and password
+load_dotenv()
+
+# get hidden username and password from local environment. See env_example for more info
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +23,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format(DB_USERNAME, DB_PASSWORD, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +41,22 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_categories(self):
+        # get all categories data
+        categories = self.client().get('/categories')
+
+        # convert data into json format
+        data = json.loads(categories.data)
+
+        # Check validity of the data
+        # Check success
+        self.assertEqual(data["success"], True)
+        # Check status code
+        self.assertEqual(categories.status_code, 200)
+        # Ensure that there are categories 
+        self.assertTrue(len(data))
+        
+
 
 
 # Make the tests conveniently executable
