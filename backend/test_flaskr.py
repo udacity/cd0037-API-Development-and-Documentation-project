617923724c8a1f8f34357e92b8d5bf8f18d0b490
fodela <particleme30@gmail.com>
2022-06-23 21:04:03 +0000

    test: get_questions_by_category

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -95,11 +95,20 @@ class TriviaTestCase(unittest.TestCase):
         res = self.client().post("/questions", json=self.new_question)
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
+        self.assertEqual(res.status_code, 200)
         self.assertTrue(data["created"])
         self.assertTrue(len(data["questions"]))
         self.assertTrue(data["total_questions"])
+
+    def test_get_questions_by_category(self):
+        res = self.client().get("/categories/1/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(data["success"], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(data["total_questions"]) 
 
 
 # Make the tests conveniently executable
