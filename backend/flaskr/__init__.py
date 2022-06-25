0ce7a944e0660e3ddf564279959a3c4dbd554c5b
fodela <particleme30@gmail.com>
2022-06-25 18:51:18 +0000

    feat: handle error 400 404 405 422

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -330,6 +330,34 @@ def create_app(test_config=None):
             except Exception as err:
                 abort(400)
 
+    """
+    @TODO:
+    Create error handlers for all expected errors
+    including 404 and 422.
+    """
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": f"bad request : {error.description}"}), 400
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": f"resource not found : {error.description}"}),
+            404,
+        )
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return (
+            jsonify({"success": False, "error": 405, "message": f"method not allowed : {error.description}"}),
+            405,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": f"unprocessable : {error.description}"}),
+            422,
+        )
 
     return app
