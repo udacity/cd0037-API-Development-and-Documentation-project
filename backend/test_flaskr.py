ccccce60b19214982d1adb6e17fb059e28fcf962
Godwin Opara <godwinopara62@yahoo.com>
2022-06-27 01:06:40 +0100

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql+psycopg2://{}:{}@{}/{}".format(
+            'student', 'student', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +25,19 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
+        self.new_question = {
+            'question': 'Who discovered penicillin?',
+            'answer': 'Alexander Fleming',
+            'category': 3,
+            'difficulty': 1
+        }
+
+        self.quizzes = {
+            'previous_questions': [1, 4, 20, 15],
+            'quiz_category': 'History'
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +46,89 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+
+    def test_invalid_request(self):
+        res = self.client().get('/categories/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+
+
+
+    def test_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+
+   
+
+    def test_retrieve_questions_by_category(self):
+        res = self.client().get('/categories/5/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['currentCategory'])
+
+  
+
+    def test_404_paginated_pages(self):
+        res = self.client().get('/questions?page=1000', json={'rating': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+
+
+    def test_new_question(self):
+        res = self.client().post('/questions', json={"searchTerm": "title"})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+
+    
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/5')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted_question'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+
+ 
+
+    def test_404_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
 
 
 # Make the tests conveniently executable
