67620767d942f5edf1d2e15d0875ca46bfdeb27e
nasro-dev <n.boukhelkhal@esi-sba.dz>
2022-09-03 00:23:35 +0100

    test-flask

--- backend/README.md
+++ backend/README.md
@@ -73,11 +73,13 @@ You will need to provide detailed documentation of your API endpoints including 
 
 ### Documentation Example
 
-`GET '/api/v1.0/categories'`
+`GET '/categories'`
 
 - Fetches a dictionary of categories in which the keys are the ids and the value is the corresponding string of the category
 - Request Arguments: None
 - Returns: An object with a single key, `categories`, that contains an object of `id: category_string` key: value pairs.
+
+Sample: curl http://127.0.0.1:5000/categories
 
 ```json
 {
@@ -89,6 +91,208 @@ You will need to provide detailed documentation of your API endpoints including 
   "6": "Sports"
 }
 ```
+
+`GET /categories/${id}/questions`
+
+- Returns a list of all questions in a category
+- If there are many questions this query will be paginated and the page argument must be used to specify the page to be returned.
+
+Sample: curl http://127.0.0.1:5000/categories/1/questions
+```
+{
+  "currentCategory": "Science", 
+  "questions": [
+    {
+      "answer": "The Liver", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 20, 
+      "question": "What is the heaviest organ in the human body?"
+    }, 
+    {
+      "answer": "Alexander Fleming", 
+      "category": 1, 
+      "difficulty": 3, 
+      "id": 21, 
+      "question": "Who discovered penicillin?"
+    }, 
+    {
+      "answer": "Blood", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 22, 
+      "question": "Hematology is a branch of medicine involving the study of what?"
+    }
+  ], 
+  "success": true, 
+  "totalQuestions": 3
+}
+```
+
+`GET /questions`
+
+- Returns a list of all questions
+- If there are many questions this query will be paginated and the page argument must be used to specify the page to be returned.
+
+Sample: curl http://127.0.0.1:5000/questions
+```
+  {
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "currentCategory": null, 
+  "questions": [
+    {
+      "answer": "Apollo 13", 
+      "category": 5, 
+      "difficulty": 4, 
+      "id": 2, 
+      "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+    }, 
+    {
+      "answer": "Tom Cruise", 
+      "category": 5, 
+      "difficulty": 4, 
+      "id": 4, 
+      "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+    }, 
+    {
+      "answer": "Maya Angelou", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 5, 
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    }, 
+    {
+      "answer": "Edward Scissorhands", 
+      "category": 5, 
+      "difficulty": 3, 
+      "id": 6, 
+      "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?"
+    }, 
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?"
+    }, 
+    {
+      "answer": "Brazil", 
+      "category": 6, 
+      "difficulty": 3, 
+      "id": 10, 
+      "question": "Which is the only team to play in every soccer World Cup tournament?"
+    }, 
+    {
+      "answer": "Uruguay", 
+      "category": 6, 
+      "difficulty": 4, 
+      "id": 11, 
+      "question": "Which country won the first ever soccer World Cup in 1930?"
+    }, 
+    {
+      "answer": "George Washington Carver", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 12, 
+      "question": "Who invented Peanut Butter?"
+    }, 
+    {
+      "answer": "Lake Victoria", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 13, 
+      "question": "What is the largest lake in Africa?"
+    }, 
+    {
+      "answer": "Agra", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 15, 
+      "question": "The Taj Mahal is located in which Indian city?"
+    }
+  ], 
+  "success": true, 
+  "totalQuestions": 10
+}
+```
+
+`POST /questions`
+
+- Creates a new question
+- The request body must contain a JSON object 
+
+Sample :  curl -X POST -H "Content-Type: application/json" -d '{"question": "What is the biggest country in the world", "answer": "Russia", "difficulty": 1, "category": 3 }' http://127.0.0.1:5000/questions
+
+```
+{
+  "message": "Question ceated successfully", 
+  "success": true
+}
+```
+
+`DELETE /questions/${id}`
+
+- Delete a question
+
+Sample:  curl -X DELETE  http://127.0.0.1:5000/questions/10
+
+```
+   {
+  "message": "Question with id:10 is deleted", 
+  "success": true
+   }
+```
+
+`POST /questions/search`
+
+- Searches for a question based on a given search term
+- The request body must contain a JSON object
+
+Sample: curl -X POST -H "Content-Type: application/json" -d '{"searchTerm": "movie"}' http://127.0.0.1:5000/questions/search
+
+```
+  {
+    "currentCategory": null, 
+    "questions": [
+      {
+        "answer": "Apollo 13", 
+        "category": 5, 
+        "difficulty": 4, 
+        "id": 2, 
+        "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+      }
+    ], 
+    "success": true, 
+    "totalQuestions": 1
+  }
+```
+
+`POST /quizzes`
+
+- Starts a new quiz based on a given category
+- The request body must contain a JSON object
+
+Sample: curl -X POST -H "Content-Type: application/json" -d '{"quiz_category": {"type": "Science", "id": 1}, "previous_questions": [1, 4, 20, 15]}' http://127.0.0.1:5000/quizzes
+
+```
+   {
+      "question": {
+        "answer": "One", 
+        "category": 2, 
+        "difficulty": 4, 
+        "id": 18, 
+        "question": "How many paintings did Van Gogh sell in his lifetime?"
+      }, 
+      "success": true
+   }
+```   
 
 ## Testing
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -25,7 +25,26 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+        self.new_question_test = {
+            'question': 'What is the capital of France?',
+            'answer': 'Paris',
+            'category': '3',
+            'difficulty': '1'
+        }    
+        self.searchTerm_test ={
+            'searchTerm': 'movie'
+        } 
+        self.searchTerm_test_error ={
+            'searchterm': 'movie'
+        } 
+        self.quiz_test = {
+            'quiz_category': {'type': "History", 'id': 6},
+            'previous_questions': [11, 23, 16],
+        }
+        self.quiz_test_error = {
+            'quiz_category': {'type': "History", 'id': 1000},
+            'previous_questions': [12, 15, 23],
+        }
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -34,7 +53,142 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+
+    def test_404_sent_request_beyond_valid_page(self):
+        res = self.client().get('/questions?page=1000',json={'rating':1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'],False)
+        self.assertEqual(data['message'],'resource not found')
+    # get all categories test
+    def test_get_all_categories(self):
+        res = self.client().get('/categories')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_404_get_all_categories(self):
+        for i in {0, 100}:
+            res = self.client().get(f'/categories?page={i}')    
+            data = res.get_json()
+            self.assertEqual(res.status_code, 404)
+            self.assertFalse(data['success'])
+            self.assertTrue(data['message'])
+
+    # get all questions test
+    def test_get_all_questions(self):
+        res = self.client().get('/questions')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['categories'])
+
+    def test_422_get_all_questions(self):
+        for i in {0, 100}:
+            res = self.client().get(f'/questions?page={i}')    
+            data = res.get_json()
+            self.assertEqual(res.status_code, 422)
+            self.assertFalse(data['success'])
+            self.assertTrue(data['message'])
+
+    def test_404_get_all_questions(self):
+        for i in {0, 100}:
+            res = self.client().get(f'/questions?page={i}')    
+            data = res.get_json()
+            self.assertEqual(res.status_code, 404)
+            self.assertFalse(data['success'])
+            self.assertTrue(data['message'])        
+    
+    # delete question test
+    def test_delete_question(self):
+        question = Question.query.filter(Question.id == 9).one_or_none()
+        res = self.client().delete('/questions/9')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], f'Question {question} is deleted')
+
+    def test_404_delete_questions(self):
+        id = 1000 
+        res = self.client().delete(f'/questions/{id}')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'])    
+    
+    # create new question test
+    def test_create_new_question(self):
+        res = self.client().post('/questions',json=self.new_question_test)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['message'])
+
+    def test_422_create_new_question(self):
+        res = self.client().post('/questions',json=self.new_question_test)
+        data = res.get_json()
+        self.assertEqual(res.status_code, 422)
+        self.assertFalse(data['success'])
+        self.assertTrue(data['message'])
+    
+    # search a given question test 
+    def test_search_question(self):
+        res = self.client().post(f'/questions/search', json=self.searchTerm_test)
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(data['success'])
+        self.assertTrue(data['message'])
+
+    def test_400_search_question(self):
+        res = self.client().post(f'/questions/search', json=self.searchTerm_test_error)
+        data = res.get_json()
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
+        self.assertTrue(data['message'])
+    
+    # get questions by category test
+    def test_get_questions_by_category(self):
+        res = self.client().get(f'/categories/5/questions')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])    
+
+    def test_404_get_questions_by_category(self):
+        res = self.client().get(f'/categories/100/questions')
+        data = res.get_json()
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'])
+        self.assertTrue(data['message'])
+    
+    # get quiz questions test
+    def test_get_quiz_questions(self):
+        res = self.client().post('/quizzes', json = self.quiz_test)
+        data = res.get_json()
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertFalse(data['question'])
+
+    def test_400_get_quiz_questions(self):     
+        res = self.client().post('/quizzes', json = self.quiz_test_error)
+        data = res.get_json()
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
+        self.assertTrue(data['message'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
