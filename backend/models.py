e99285543e20dff0f199e0c7be3db4da337ae474
Baribeop Barry Leele <68976451+Baribeop@users.noreply.github.com>
2022-09-19 20:33:00 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgresql://postgres:beopson@localhost:5432/trivia'
 
 db = SQLAlchemy()
 
