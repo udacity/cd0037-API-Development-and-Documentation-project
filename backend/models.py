c63307e33ea44b78a0f72f7b6257426bbcd1035f
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2024-09-08 18:07:02 +0700

    feature/1-init-project

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgresql://postgres:12345@{}/{}'.format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -17,7 +17,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 """
 Question
